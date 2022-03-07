d4f134e9a13c6febf31b8a0a153e58978e1dfb09
Muhammad Gilbert <muhammad_gilbert_29rpl@student.smktelkom-mlg.sch.id>
2022-03-07 20:45:51 +0700

    first commit

---
+++ src/component/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+
+const Home = () => {
+    return (
+        <div className="dashboard ms-5">
+                    <p className='location-header'>Practicum 1/Home</p>
+                    <br></br><br></br><br></br><br></br><br/><br/><br/>
+                            <p className="card-text lead fs-2 mb-0">PRAKTIKUM 1</p>
+                    <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+
+                  
+                  
+            </div>
+        
+        
+    );
+};
+
+export default Home;
